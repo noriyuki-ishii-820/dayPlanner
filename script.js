51624a7c94906b1ac1c9b1f6f63741b7dab075cb
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-09-12 16:34:32 +1000

    progress

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ currentTimeText.textContent = currentTime;
 
 var currentHour = moment().format('L');   
 
+// userInput
+var userInput = document.getElementsByClassName("textBox");
+
 // time variables
 
 var hourArray = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
@@ -16,22 +19,25 @@ $(document).ready(function() {
     for (var i = 0; i < hourArray.length; i++) {
 
         $(".container").append(
-            '<div class="row"><div class="col-2 hour">' + hourArray[i] + '</div><div class="col-8 textarea"><input class="textBox"></input></div><div class="col-2 saveBtn"><i class="fas fa-save fa-vc"></i></div></div>'
+            '<div class="row"><div class="col-2 hour">' + hourArray[i] + '</div><div class="col-8 textarea"><input type="text" class="textBox"></input></div><div class="col-2 saveBtn"><i class="saveBtnIcon fas fa-save fa-vc"></i></div></div>'
           );
 
-        
-        //    if (i < 3 ) {
-        //         var hourOutput = hourArray[i] + amPm[0];            
-        //         $('.hour').html(hourOutput);
-        //         console.log("Morning" + hourOutput);
-        //     } else {
-        //         var hourOutput = hourArray[i] + amPm[1];
-        //         $('.hour').html(hourOutput);
-        //         console.log(hourOutput);
-        //  }
-     }}
+     }
      
-     );
+     function saveData(){
+        localStorage.setItem("duty", JSON.stringify(userInput));
+        alert("data saved!");
+        console.log("clicked!!");
+    }
+
+
+    $(".fa-save").click(saveData)
+
+
+
+
+
+});
 
 
    
