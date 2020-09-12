079f54fd6ad97f889073adc1729c393a619e34ad
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-09-12 13:44:42 +1000

    SaveButton done

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ var currentTimeText = document.getElementById("currentDay");
 var currentTime = moment().format('MMMM Do YYYY');
 currentTimeText.textContent = currentTime;
 
+// show the Save Icon
+
+$('.saveBtn').html('<i class="fas fa-save fa-vc"></i>');
