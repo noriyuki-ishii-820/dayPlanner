5c66ede710050be363900451137c3c776f7c473a
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-09-12 16:07:01 +1000

    basic numbers done

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //showing the current day/month
 
 var currentTimeText = document.getElementById("currentDay");
-var currentTime = moment().format('MMMM Do YYYY');
+var currentTime = moment().format("MMMM Do YYYY");
 currentTimeText.textContent = currentTime;
 
-// show the Save Icon
+var currentHour = moment().format('L');   
 
-$('.saveBtn').html('<i class="fas fa-save fa-vc"></i>');
+// time variables
+
+var hourArray = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
+
+$(document).ready(function() {
+
+ 
+    for (var i = 0; i < hourArray.length; i++) {
+
+        $(".container").append(
+            '<div class="row"><div class="col-2 hour">' + hourArray[i] + '</div><div class="col-8 textarea"><input class="textBox"></input></div><div class="col-2 saveBtn"><i class="fas fa-save fa-vc"></i></div></div>'
+          );
+
+        
+        //    if (i < 3 ) {
+        //         var hourOutput = hourArray[i] + amPm[0];            
+        //         $('.hour').html(hourOutput);
+        //         console.log("Morning" + hourOutput);
+        //     } else {
+        //         var hourOutput = hourArray[i] + amPm[1];
+        //         $('.hour').html(hourOutput);
+        //         console.log(hourOutput);
+        //  }
+     }}
+     
+     );
+
+
+   
